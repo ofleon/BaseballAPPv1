e3532005fc784897d5b68947b1df59a36d01220c
Oscar Leon <oleon@sappkinsystems.com>
2024-09-04 20:00:37 -0400

    Implementing a base api project

---
+++ BaseballAPP.API/Controllers/TeamController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseballAPP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamController : ControllerBase
+    {
+        private List<string> Players = [];
+
+        [HttpGet("GetNamePlayer")]
+        public string GetNameofPlayer()
+        {
+            return "Alex";
+        }
+
+        [HttpGet("GetListPlayers")]
+        public List<string> GetPlayerGetNameofPlayersofPlayer()
+        {
+            return Players;
+        }
+
+
+        [HttpPost]
+        public bool CreateNewPlayer(string Name)
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                return false;
+            }
+
+            Players.Add(Name);
+            return true;
+        }
+    }
+}
