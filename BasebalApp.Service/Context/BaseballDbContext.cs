c6245d02fdc556119547e8518d16a40614b9b803
Oscar Leon <oleon@sappkinsystems.com>
2024-09-11 20:47:37 -0400

    Implementing Entity Framework

---
+++ BasebalApp.Service/Context/BaseballDbContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using BasebalApp.Service.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasebalApp.Service.Context;
+
+public partial class BaseballDbContext : DbContext
+{
+    public BaseballDbContext()
+    {
+    }
+
+    public BaseballDbContext(DbContextOptions<BaseballDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Player> Players { get; set; }
+
+    public virtual DbSet<Team> Teams { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=DEVMO-PC\\MSSQLSERVER01;Database=BasebalAPP;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Player>(entity =>
+        {
+            entity.ToTable("Player");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Position)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            //entity.HasOne(d => d.Team).WithMany(p => p.Players)
+            //    .HasForeignKey(d => d.TeamId)
+            //    .HasConstraintName("FK_Player_Team");
+        });
+
+        modelBuilder.Entity<Team>(entity =>
+        {
+            entity.ToTable("Team");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ BasebalApp.Service/Entities/Player.cs
@@ -0,0 +1,15 @@
+﻿using BasebalApp.Service.Common;
+
+namespace BasebalApp.Service.Entities;
+
+public partial class Player : BaseEntity
+{
+    public string Name { get; set; } = null!;
+
+    public string Position { get; set; } = null!;
+
+    public int? TeamId { get; set; }
+
+
+    //public virtual Team? Team { get; set; }
+}
